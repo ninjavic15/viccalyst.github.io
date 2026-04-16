1c145d21a05b7c22a4fcd3e964b023c05d69c2c4
ninjavic15 <ninjavic15@gmail.com>
2026-04-16 23:25:39 +0200

    Moving background, Hover effect, Linked pages

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('Webpage Initialized.');
 
     const interactBtn = document.getElementById('interactiveBtn');
-    
+
     if (interactBtn) {
         interactBtn.addEventListener('click', () => {
             // A simple subtle micro-animation response when clicked
             interactBtn.style.transform = 'scale(0.92)';
-            
+
             setTimeout(() => {
                 interactBtn.style.transform = '';
             }, 100);
-            
+
             console.log('Interaction logged.');
         });
     }
